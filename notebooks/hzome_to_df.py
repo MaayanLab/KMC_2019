6453e407bbe9b32c92ef2c92ce9f2757b01511e6
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-07 18:08:36 -0500

    made hzome_to_df function to load hzome data into my normal dataframe format

---
+++ notebooks/hzome_to_df.py
@@ -0,0 +1,30 @@
+def main(filename):
+
+  print('here')
+  import pandas as pd
+
+  hzome = pd.read_csv(filename, sep='\t')
+
+  # set the index (row names) to the first column
+  hzome = hzome.set_index('#')
+
+  # drop the two other columns
+  hzome = hzome.drop('#.1', 1)
+  hzome = hzome.drop('GeneSym', 1)
+
+  # drop two row indexes
+  hzome = hzome.drop( hzome.index[[0,1]] )
+
+  # make new dataframe from the pieces
+  rows = hzome.index.tolist()
+  cols = hzome.columns.tolist()
+  mat = hzome.as_matrix()
+
+  df = pd.DataFrame(data=mat, columns=cols, index=rows)
+
+  return df
+
+# # how to run
+# #############
+# filename = '../hzome_data/small_encode.txt'
+# df = main(filename)
