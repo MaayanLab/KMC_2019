e2f800766c4de26822f47196440c961c4f939f64
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-07 15:16:13 -0500

    working on comparing grant-dark-genes and all genes from harmonogram

--- notebooks/load_hgram_gene_info.py
+++ notebooks/load_hgram_gene_info.py
@@ -3,11 +3,21 @@ def main():
 
   hgram_info = json_scripts.load_to_dict('../harmonogram_classes/gene_classes_harmonogram.json')
 
+  grant_poi = json_scripts.load_to_dict('../grant_pois/proteins_of_interest.json')
+
   gene_types = ['KIN', 'IC', 'GPCR']
 
   for inst_type in gene_types:
+
+    all_genes = hgram_info[inst_type]
+    dark_genes = grant_poi[inst_type]
+
     print(inst_type)
-    print(len(hgram_info[inst_type]))
+    print('all: ' + str(len(all_genes)))
+    print('dark: ' + str(len(dark_genes)))
     print('\n')
+
+
+
 
 main()
